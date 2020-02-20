6b80956a64fa6880f009b371433c8c799052d652
Aftab Alam <aftab.alam@jdplc.com>
2020-02-20 16:31:47 +0000

    first commit

---
+++ app.js
@@ -0,0 +1,112 @@
+(document).ready(function() {
+
+  window.setInterval(function(){
+    
+      var elem = document.querySelector('.picture1');
+      var elem2 = document.querySelector('.picture2');
+      var bounding = elem.getBoundingClientRect();
+      var bounding2 = elem2.getBoundingClientRect();
+
+      var elem3 = document.querySelector('.picture3');
+      var elem4 = document.querySelector('.picture4');
+      var bounding3 = elem3.getBoundingClientRect();
+      var bounding4 = elem4.getBoundingClientRect();
+
+      var elem5 = document.querySelector('.picture5');
+      var elem6 = document.querySelector('.picture6');
+      var elem7 = document.querySelector('.picture7');
+      var bounding5 = elem5.getBoundingClientRect();
+      var bounding6 = elem6.getBoundingClientRect();
+      var bounding7 = elem7.getBoundingClientRect();
+
+      var elem8 = document.querySelector('.picture8');
+      var elem9 = document.querySelector('.picture9');
+      var elem10 = document.querySelector('.picture10');
+      var elem11 = document.querySelector('.picture11');
+      var elem12 = document.querySelector('.picture12');
+      var elem13 = document.querySelector('.picture13');
+      var elem14 = document.querySelector('.picture14');
+      var elem15 = document.querySelector('.picture15');
+      var elem16 = document.querySelector('.picture16');
+      var bounding8 = elem8.getBoundingClientRect();
+      var bounding9 = elem9.getBoundingClientRect();
+      var bounding10 = elem10.getBoundingClientRect();
+      var bounding11 = elem11.getBoundingClientRect();
+      var bounding12 = elem12.getBoundingClientRect();
+      var bounding13 = elem13.getBoundingClientRect();
+      var bounding14 = elem14.getBoundingClientRect();
+      var bounding15 = elem15.getBoundingClientRect();
+      var bounding16 = elem16.getBoundingClientRect();
+      var elem17 = document.querySelector('.picture17');
+      var bounding17 = elem17.getBoundingClientRect();
+
+      var elem18 = document.querySelector('.picture18');
+      var bounding18 = elem18.getBoundingClientRect();
+    
+    
+
+    
+    if (bounding.x === 8){
+      console.log('2000');
+    }
+    else if (bounding2.x === 8){
+      console.log('2000');
+    }
+    else if (bounding3.x === 8){
+      console.log('2005');
+    }
+    else if (bounding4.x === 8){
+      console.log('2005');
+    }
+    else if (bounding5.x === 8){
+      console.log('2010');
+    }
+    else if (bounding6.x === 8){
+      console.log('2010');
+    }
+    else if (bounding7.x === 8){
+      console.log('2010');
+    }
+    else if (bounding8.x === 8){
+      console.log('2015');
+    }
+    else if (bounding9.x === 8){
+      console.log('2015');
+    }
+    else if (bounding10.x === 8){
+      console.log('2015');
+    }
+    else if (bounding11.x === 8){
+      console.log('2015');
+    }
+    else if (bounding12.x === 8){
+      console.log('2015');
+    }
+    else if (bounding13.x === 8){
+      console.log('2015');
+    }
+    else if (bounding14.x === 8){
+      console.log('2015');
+    }
+    else if (bounding15.x === 8){
+      console.log('2015');
+    }
+    else if (bounding16.x === 8){
+      console.log('2015');
+    }
+    else if (bounding17.x === 8){
+      console.log('2015');
+    }
+    else{
+      console.log('2020');
+    }
+  }, 1000);
+
+
+  
+  
+});
+
+
+
+
